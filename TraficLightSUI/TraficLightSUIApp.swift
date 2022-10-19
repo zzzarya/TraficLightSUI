5cd78d7bb5e62f4a70e99dfacc11a7b3e03c0b9f
Anton Zarichny <antonzarichny@gmail.com>
2022-10-19 18:28:30 +0300

    Initial Commit

---
+++ TraficLightSUI/TraficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightSUIApp.swift
+//  TraficLightSUI
+//
+//  Created by Антон Заричный on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
